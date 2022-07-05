a1249d81fcdbf87b4c9d375cd91f2c5912257afb
sgsg <519117694@qq.com>
2022-07-05 22:32:11 +0800

    ui

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿
+namespace ftpWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ip_input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.port_input = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.TextBox();
+            this.pwd = new System.Windows.Forms.TextBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.folder = new System.Windows.Forms.TextBox();
+            this.selectFolder = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.upload = new System.Windows.Forms.Button();
+            this.download = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP：";
+            // 
+            // ip_input
+            // 
+            this.ip_input.Location = new System.Drawing.Point(83, 40);
+            this.ip_input.Name = "ip_input";
+            this.ip_input.Size = new System.Drawing.Size(100, 25);
+            this.ip_input.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port：";
+            // 
+            // port_input
+            // 
+            this.port_input.Location = new System.Drawing.Point(283, 40);
+            this.port_input.Name = "port_input";
+            this.port_input.Size = new System.Drawing.Size(100, 25);
+            this.port_input.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(406, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(606, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Password：";
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(483, 40);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(100, 25);
+            this.user.TabIndex = 6;
+            // 
+            // pwd
+            // 
+            this.pwd.Location = new System.Drawing.Point(715, 40);
+            this.pwd.Name = "pwd";
+            this.pwd.Size = new System.Drawing.Size(108, 25);
+            this.pwd.TabIndex = 7;
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(846, 34);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 33);
+            this.connect.TabIndex = 8;
+            this.connect.Text = "连接";
+            this.connect.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.connect);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.ip_input);
+            this.panel1.Controls.Add(this.pwd);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.user);
+            this.panel1.Controls.Add(this.port_input);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(935, 100);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.download);
+            this.panel2.Controls.Add(this.upload);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(935, 570);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listBox3);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 423);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(935, 147);
+            this.panel3.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "本地：";
+            // 
+            // folder
+            // 
+            this.folder.Location = new System.Drawing.Point(67, 11);
+            this.folder.Name = "folder";
+            this.folder.Size = new System.Drawing.Size(308, 25);
+            this.folder.TabIndex = 2;
+            this.folder.TextChanged += new System.EventHandler(this.folder_TextChanged);
+            // 
+            // selectFolder
+            // 
+            this.selectFolder.Location = new System.Drawing.Point(381, 11);
+            this.selectFolder.Name = "selectFolder";
+            this.selectFolder.Size = new System.Drawing.Size(42, 28);
+            this.selectFolder.TabIndex = 3;
+            this.selectFolder.Text = "...";
+            this.selectFolder.UseVisualStyleBackColor = true;
+            this.selectFolder.Click += new System.EventHandler(this.selectFolder_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.folder);
+            this.panel4.Controls.Add(this.selectFolder);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Location = new System.Drawing.Point(12, 15);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(426, 50);
+            this.panel4.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(508, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(363, 383);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(75, 33);
+            this.upload.TabIndex = 9;
+            this.upload.Text = "上传";
+            this.upload.UseVisualStyleBackColor = true;
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(847, 384);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(75, 33);
+            this.download.TabIndex = 10;
+            this.download.Text = "下载";
+            this.download.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "状态：";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(25, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(410, 304);
+            this.listBox1.TabIndex = 11;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(511, 74);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(410, 304);
+            this.listBox2.TabIndex = 12;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(15, 28);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(906, 109);
+            this.listBox3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 670);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(953, 717);
+            this.MinimumSize = new System.Drawing.Size(953, 717);
+            this.Name = "Form1";
+            this.Text = "FTPClient";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ip_input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox port_input;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pwd;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button download;
+        private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox folder;
+        private System.Windows.Forms.Button selectFolder;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace ftpWindows
+{
+    public partial class Form1 : Form
+    {
+        private bool localFolderTag = false;
+
+        
+        private TcpClient cmdServer;
+        private TcpClient dataServer;
+        private NetworkStream cmdStrmWtr;
+        private StreamReader cmdStrmRdr;
+        private NetworkStream dataStrmWtr;
+        private StreamReader dataStrmRdr;
+        private String cmdData;
+        private byte[] szData;
+        private const String CRLF = "\r\n";
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folder_TextChanged(object sender, EventArgs e)
+        {
+            string path = folder.Text;
+            string pattern = "*";
+            try
+            {
+                List<FileInfo> folder = new DirectoryInfo(path).GetFiles(pattern).ToList();
+                this.listBox1.Items.Clear();
+                foreach (FileInfo file in folder)
+                {
+                    this.listBox1.Items.Add(file.Name);
+                }
+                localFolderTag = true;
+            }
+            catch
+            {
+                this.listBox1.Items.Clear();
+                this.listBox1.Items.Add("文件夹打开失败");
+                localFolderTag = false;
+            }
+            
+        }
+
+        private void selectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择本地文件夹";  //提示的文字
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                this.folder.Text = folder.SelectedPath;
+            }
+        }
+    }
+}
